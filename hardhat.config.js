adf434e32983ba6ce4c3249c9578b1d71a2bd942
Llydia Cross <llydiacross@gmail.com>
2023-02-20 04:32:59 +0000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,34 @@
-require("module-alias/register");
+require('module-alias/register');
+
 const {
-  debugLog,
-  loadInfinityMint,
-  prepareConfig,
-} = require("infinitymint/dist/app/helpers");
+    debugLog,
+    loadInfinityMint,
+    prepareConfig,
+} = require('infinitymint/dist/app/helpers');
+//create the default pipe
+const { createDefaultFactory } = require('infinitymint/dist/app/pipes');
+
+//set as javascript session
+let session = readSession();
+session.environment.javascript = true;
+saveSession(session);
+
+createDefaultFactory();
 
 //require dotenv
-require("dotenv").config({
-  override: false, //will not override already established environment variables
+require('dotenv').config({
+    override: false, //will not override already established environment variables
 });
 
 //import our hardhat plugins
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-ethers");
-require("hardhat-change-network"); //allows hre.changeNetwork to occur
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-change-network'); //allows hre.changeNetwork to occur
 
 //load infinitymint
-loadInfinityMint(true);
+loadInfinityMint(true, false);
 
 //return the infinitymint config file
 let config = prepareConfig();
-
-debugLog("loaded hardhat.config.js");
+debugLog('loaded hardhat.config.js');
 module.exports = config.hardhat; //export the infinity mint configuration file
